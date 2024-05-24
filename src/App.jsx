a9753a4699055dc7a6b86ecd9b5528d1a732eb34
Brittney Epperson <beppers87@hotmail.com>
2024-05-24 12:51:57 -0500

    Added basic calculator structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,127 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div>
-        React Calculator
-      </div>
-    </>
-  )
+		<div id='app'>
+			<h1>React Calculator</h1>
+			<div id='calculator'>
+				<div id='display'>
+          <div id='display-line'></div>
+          <div id='input-line'></div>
+        </div>
+				<div id='buttons'>
+					<button
+						id='clear'
+						value='AC'
+						className='jumbo'
+					>
+						AC
+					</button>
+					<button
+						id='divide'
+						value='/'
+						className='operator'
+					>
+						/
+					</button>
+					<button
+						id='multiply'
+						value='x'
+						className='operator'
+					>
+						X
+					</button>
+					<button
+						id='seven'
+						value='7'
+					>
+						7
+					</button>
+					<button
+						id='eight'
+						value='8'
+					>
+						8
+					</button>
+					<button
+						id='nine'
+						value='9'
+					>
+						9
+					</button>
+					<button
+						id='subtract'
+						value='-'
+						className='operator'
+					>
+						-
+					</button>
+					<button
+						id='four'
+						value='4'
+					>
+						4
+					</button>
+					<button
+						id='five'
+						value='5'
+					>
+						5
+					</button>
+					<button
+						id='six'
+						value='6'
+					>
+						6
+					</button>
+					<button
+						id='add'
+						value='+'
+						className='operator'
+					>
+						+
+					</button>
+					<button
+						id='one'
+						value='1'
+					>
+						1
+					</button>
+					<button
+						id='two'
+						value='2'
+					>
+						2
+					</button>
+					<button
+						id='three'
+						value='3'
+					>
+						3
+					</button>
+					<button
+						id='zero'
+						value='0'
+						className='jumbo'
+					>
+						0
+					</button>
+					<button
+						id='decimal'
+						value='.'
+						className='operator'
+					>
+						.
+					</button>
+					<button
+						id='equals'
+						value='='
+					>
+						=
+					</button>
+				</div>
+			</div>
+		</div>
+  );
 }
 
 export default App
